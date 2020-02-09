f096f58d960966e9f8f73bdfc1353fdc69c995a3
Shunya78 <kikukan007@gmail.com>
2020-02-09 23:26:44 +0900

    Comp bss

--- app.rb
+++ app.rb
@@ -6,8 +6,12 @@ require 'sinatra/json'
 require './models/contribution.rb'
 
 before do
-
-
+  Dotenv.load
+    Cloudinary.config do |config|
+        config.cloud_name = ENV['CLOUD_NAME']
+        config.api_key = ENV['CLOUDINARY_API_KEY']
+        config.api_secret = ENV['CLOUDINARY_API_SECRET']
+    end
 end
 
 get '/' do
@@ -16,10 +20,49 @@ get '/' do
 end
 
 post '/new' do
+  img_url = ''
+    if params[:file]
+        img = params[:file]
+        tempfile = img[:tempfile]
+        upload = Cloudinary::Uploader.upload(tempfile.path)
+        img_url = upload['url']
+    end
+
     Contribution.create({
+        name: params[:user_name],
+        body: params[:body],
+        img: img_url
+    })
+
+
+    redirect '/'
+end
+
+post '/delete/:id' do
+    Contribution.find(params[:id]).destroy
+    redirect '/'
+end
+
+get '/edit/:id' do
+    @content = Contribution.find(params[:id])
+    erb :edit
+end
+
+post '/renew/:id' do
+    content = Contribution.find(params[:id])
+    content.update({
         name: params[:user_name],
         body: params[:body]
     })
-
     redirect '/'
-  end
+end
+
+post '/good/:id' do
+    content = Contribution.find(params[:id])
+    good = content.good
+    content.update({
+        good: good + 1
+    })
+    redirect '/'
+    redirect '/'
+end
--- db/schema.rb
+++ db/schema.rb
@@ -10,13 +10,15 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2020_02_09_130951) do
+ActiveRecord::Schema.define(version: 2020_02_09_141309) do
 
   create_table "contributions", force: :cascade do |t|
     t.string "name"
     t.string "body"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.integer "good", default: 0
+    t.string "img"
   end
 
 end
